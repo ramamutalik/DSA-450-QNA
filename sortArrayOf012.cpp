7179de92bc1dc25afcdbf417cf95b9b9b02168d2
Rama Mutalikdesai <68442061+ramamutalik@users.noreply.github.com>
2021-11-23 21:50:17 +0530

    Create sortArrayOf012.cpp

---
+++ sortArrayOf012.cpp
@@ -0,0 +1,66 @@
+/*******************************************
+
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+Example 1:
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+Example 2:
+
+Input: 
+N = 3
+arr[] = {0 1 0}
+Output:
+0 0 1
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 <= N <= 10^6
+0 <= A[i] <= 2
+
+********************************************/
+
+void sort012(int a[], int n)
+    {
+        // coode here 
+        int arr[n],l=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==0){
+                arr[l]=a[i];
+                l++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]==1){
+                arr[l]=a[i];
+                l++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]==2){
+                arr[l]=a[i];
+                l++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            a[i]=arr[i];
+        }
+    }
