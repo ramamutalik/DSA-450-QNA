56280483bead0d3ef174150b8fdb1cf3d56e8a78
Rama Mutalikdesai <68442061+ramamutalik@users.noreply.github.com>
2022-02-09 20:22:52 +0530

    Update moveAllNegEle.cpp

--- moveAllNegEle.cpp
+++ moveAllNegEle.cpp
@@ -62,3 +62,33 @@ void segregateElements(int arr[],int n)
             }
         }
     }
+
+
+
+/*****approach***/
+
+#include <bits/stdc++.h>
+using namespace std;
+void rearrange(int arr[], int n){
+    int i=-1;
+    int pivot=0;
+    for(int j=0;j<n;j++){
+        if(arr[j]<pivot){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+}
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+}
+int main() {
+	//code
+	int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    rearrange(arr, n);
+    printArray(arr, n);
+	return 0;
+}
