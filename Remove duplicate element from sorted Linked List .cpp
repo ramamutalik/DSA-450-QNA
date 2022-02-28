e9f5ac5b7663ee178e58b82e735870ac6e774c87
Rama Mutalikdesai <68442061+ramamutalik@users.noreply.github.com>
2022-03-01 00:43:28 +0530

    Create Remove duplicate element from sorted Linked List .cpp

---
+++ Remove duplicate element from sorted Linked List .cpp
@@ -0,0 +1,14 @@
+Node *removeDuplicates(Node *head)
+{
+ // your code goes here
+ Node* temp=head;
+ while(temp!=NULL&&temp->next!=NULL){
+     if(temp->data==temp->next->data){
+         temp->next=temp->next->next;
+     }
+     else{
+         temp=temp->next;
+     }
+ }
+ return head;
+}
