c215ee02c12befadedeeb0fc9191c27d8a37f04d
Rama Mutalikdesai <68442061+ramamutalik@users.noreply.github.com>
2022-05-31 12:52:17 +0530

    Create approach2.cpp

---
+++ Array/4SUM/approach2.cpp
@@ -0,0 +1,7 @@
+//aproach 2
+------------------------Note---------------
+we can use set DS to avoid duplicates in the ans DS..... 
+    By using :
+vector<vector<int>> res(sv.begin(),sv.end());
+------------------------------------------
+ 
