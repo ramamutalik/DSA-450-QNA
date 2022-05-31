d42dff6bc277a0674bea153d1de9304a2b563665
Rama Mutalikdesai <68442061+ramamutalik@users.noreply.github.com>
2022-05-31 12:51:42 +0530

    Update 4sum.cpp

--- Array/4SUM/4sum.cpp
+++ Array/4SUM/4sum.cpp
@@ -51,9 +51,3 @@ public:
 };
 
 
-//aproach 2
-------------------------Note---------------
-we can use set DS to avoid duplicates in the ans DS..... 
-    By using :
-vector<vector<int>> res(sv.begin(),sv.end());
-------------------------------------------
