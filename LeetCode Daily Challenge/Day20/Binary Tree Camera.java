e1d418fe7e3f81369ee4ec6c2877066cce255c1d
Rama Mutalikdesai <68442061+ramamutalik@users.noreply.github.com>
2022-06-17 13:20:26 +0530

    Create Binary Tree Camera.java

---
+++ LeetCode Daily Challenge/Day20/Binary Tree Camera.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int cam;
+    Set<TreeNode> covered;
+    public int minCameraCover(TreeNode root) {
+        if(root == null) return 0;
+        cam = 0;
+        covered = new HashSet<>();
+        covered.add(null); // Skip the leaf nodes and start from one level above.
+        dfs(root, null);
+        return cam;
+    }
+    
+    public void dfs(TreeNode node, TreeNode parent){
+        if(node != null) {
+            dfs(node.left, node);
+            dfs(node.right, node);
+            
+            //Check if i need to add camera at node. 
+            // Parent is null & node is uncovered 
+            // OR 
+            // if any of its left or right child are not covered
+            
+            if(parent == null && !covered.contains(node)
+               || !covered.contains(node.left) || !covered.contains(node.right)) {
+                cam++;
+                covered.add(node);
+                covered.add(parent);
+                covered.add(node.left);
+                covered.add(node.right);
+            }
+            
+        }
+    }
+}
