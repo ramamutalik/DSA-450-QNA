8c852cde26b354fc2ffa1e3a0ff13cdb80dde08e
Rama Mutalikdesai <68442061+ramamutalik@users.noreply.github.com>
2022-07-24 11:01:54 +0530

    Create dia.cpp

---
+++ Binary Tree/Diameter of BT/dia.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findMax(TreeNode* node,int & maxi){
+        if(node == NULL){
+            return 0;
+        }
+        int lh=findMax(node->left,maxi);
+        int rh=findMax(node->right,maxi);
+        maxi=max(maxi,lh+rh);
+        return 1+max(lh,rh);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        
+        int maxi=0;
+        int height=findMax(root,maxi);
+        return maxi;
+    }
+};
